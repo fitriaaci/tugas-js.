7e2d2a537e581f3dce38a65ca34cf3b84ccc8449
fitriaaci <fitriaaci13@gmail.com>
2025-02-11 20:22:09 -0800

    Add files via upload

---
+++ variabel.js
@@ -0,0 +1,37 @@
+// variebel dengan let 
+let nama = "Agus Salim"
+
+//tampilkan data
+console.log(tersangka);
+
+//kita ubah nilai variabel nama
+let tersangka = "Agus Buntung";
+console.log(tersangka);
+
+//2. variabel dengan var
+var namaDepan ="Gus";
+console.log(namaDepan);
+namaDepan="Manaulana";
+console.log(namaDepan);
+
+//bedanya dengan led
+let jajan="Seblak"
+{
+    let jajan = "Ayam Geprek"
+    console.log(jajan);
+}
+    console.log(jajan);
+
+    //jika dengan var
+    let traktir = "Bakso Mercon"
+{
+    let traktir ="es teh jumbo";
+    console.log(traktir);
+}
+console.log(traktir);
+
+//variabel dengan const
+conts TTL ="1 Januari 2025";
+console.log(TTL)
+//const TTL ="1 Pebruari 2025"
+//ini tidak boleh dilakukan karena cont nilainya tetap
